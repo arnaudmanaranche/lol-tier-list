150183e0a9feaca75a8c24b2f2d4a738be807ac0
arnaudmanaranche <arnaud@onrewind.com>
2021-05-04 17:46:41 +0200

    Add link to status page

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,11 +1,18 @@
 import Link from 'next/link'
 
 const Footer: React.FC = () => (
-  <footer className="flex items-center justify-center h-20">
+  <footer className="flex flex-col items-center">
+    <p className="mb-3">
+      <Link href="https://lolpowerranking.statuspage.io/">
+        <a target="_blank">Status</a>
+      </Link>
+    </p>
     <p>
       Stats provided by{' '}
       <Link href="https://pandascore.co/">
-        <a className="text-primary">PandaScore</a>
+        <a className="text-primary" target="_blank">
+          PandaScore
+        </a>
       </Link>
     </p>
   </footer>
