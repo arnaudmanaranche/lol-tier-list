3c2f5426937795877f4ae158ce8b447be14dcf84
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2022-02-10 21:27:34 +0100

    Type responses from API routes

--- src/pages/api/ranking/delete.ts
+++ src/pages/api/ranking/delete.ts
@@ -1,9 +1,10 @@
+import { Ranking } from '@prisma/client'
 import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import prisma from 'Utils/prisma'
 
-async function deleteRanking(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+async function deleteRanking(req: NextApiRequest, res: NextApiResponse<Ranking>): Promise<void> {
   const { rankingId } = req.body
 
   const ranking = await prisma.ranking.delete({
--- src/pages/api/ranking/new.ts
+++ src/pages/api/ranking/new.ts
@@ -1,9 +1,10 @@
+import { Ranking } from '@prisma/client'
 import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import prisma from 'Utils/prisma'
 
-async function createRanking(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+async function createRanking(req: NextApiRequest, res: NextApiResponse<Ranking>): Promise<void> {
   const { ranking, tournamentId, userId } = req.body
 
   const result = await prisma.ranking.create({
