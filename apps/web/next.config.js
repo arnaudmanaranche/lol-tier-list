27dc7e899552e7abcec84f9321a981c39e29426a
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2022-04-20 17:09:57 +0200

    fixup! Edit next.config.js
    Edit next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,9 +7,6 @@ const withTM = require('next-transpile-modules')(['@lpr/ui', '@lpr/types'])
  */
 const moduleExports = {
   swcMinify: true,
-  future: {
-    strictPostcssConfiguration: true
-  },
   reactStrictMode: true,
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
