b44d8aaa60561c8a4a28866a246af064049fd264
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2022-08-10 17:05:32 +0200

    fix: forgot redis default import

--- apps/web/src/utils/api/rankings/deleteRanking.ts
+++ apps/web/src/utils/api/rankings/deleteRanking.ts
@@ -1,5 +1,5 @@
 import prisma from 'Utils/prisma'
-import redis from 'Utils/redis'
+import { redis } from 'Utils/redis'
 
 export async function deleteRanking(id: string): Promise<void> {
   const ranking = await prisma.ranking.delete({
