b2b14eeb03368d46ab524114250b4e7b24f61384
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2022-09-06 13:36:54 +0200

    Add async headers for enable CORS

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -21,6 +21,14 @@ const moduleExports = {
       {
         source: '/(.*)',
         headers: securityHeaders
+      },
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' }
+        ]
       }
     ]
   },
