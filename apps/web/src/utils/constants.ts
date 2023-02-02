09af6f97be49c637a7c498e51431c2e626e9980f
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-02 16:24:01 +0100

    fix: update api preview url

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -19,6 +19,6 @@ export const SUPABASE_EVENTS = {
 
 export const API_ENDPOINT = {
   development: 'http://localhost:3000/api',
-  preview: 'https://preview.lol-power-ranking.app/api',
+  preview: 'https://lol-power-ranking.vercel.app/api',
   production: 'https://lol-power-ranking.app/api'
 }[process.env.NEXT_PUBLIC_APP_ENV]
