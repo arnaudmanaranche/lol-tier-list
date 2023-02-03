07ceb43cf2ee43c711fcb5ea93b21ac4bf664314
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-03 09:02:53 +0100

    fix: log urls

--- apps/web/src/utils/api.ts
+++ apps/web/src/utils/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+console.log(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`)
+
 const API_ENDPOINT = {
   development: 'http://localhost:3000/api',
   preview: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`,
