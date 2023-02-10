c745ed93785b74b1223059b5b687b4f5ebf12a0e
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-10 09:41:19 +0100

    feat: run next image codemod

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { m } from 'framer-motion'
-import Image from 'next/image'
+import Image from "next/legacy/image";
 import type { ReactElement } from 'react'
 
 import { Button, Title } from '@lpr/ui'
