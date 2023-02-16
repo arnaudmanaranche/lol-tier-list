0ee2894288dd59fe6d477f09b3e5d14bc1e5621c
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-16 23:06:06 +0100

    fix: edit env variables

--- apps/web/custom.d.ts
+++ apps/web/custom.d.ts
@@ -4,10 +4,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     DATABASE_URL: string
     NEXT_PUBLIC_APP_ENV: string
-    NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY: string
     NEXT_PUBLIC_CRONITOR_API_KEY: string
     NEXT_PUBLIC_SENTRY_DSN: string
     NEXT_PUBLIC_SUPABASE_ID: string
+    NEXT_PUBLIC_SUPABASE_URL: string
     NEXT_PUBLIC_SUPABASE_TOKEN: string
     PANDASCORE_TOKEN: string
     REDIS_URL: string
