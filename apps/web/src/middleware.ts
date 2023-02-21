a2368d958583362ac34993572b97b11e4097f6e7
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-21 08:12:04 +0100

    fix: temporary disable middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,26 +1,29 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+// import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+// import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-import { isProtectedRouteWithProtectedMethod } from 'Utils/isProtectedRouteWithProtectedMethod'
+// import { isProtectedRouteWithProtectedMethod } from 'Utils/isProtectedRouteWithProtectedMethod'
 
-export async function middleware(req: NextRequest, res: NextResponse) {
-  const supabaseClient = createMiddlewareSupabaseClient({ req, res })
+// export async function middleware(req: NextRequest, res: NextResponse) {
+//   const supabaseClient = createMiddlewareSupabaseClient({ req, res })
 
-  const {
-    data: { session }
-  } = await supabaseClient.auth.getSession()
+//   const {
+//     data: { session }
+//   } = await supabaseClient.auth.getSession()
 
-  if (!session && isProtectedRouteWithProtectedMethod(req.nextUrl.pathname, req.method)) {
-    return new NextResponse(JSON.stringify({ success: false, message: 'Unauthorized' }), {
-      status: 401,
-      headers: { 'content-type': 'application/json' }
-    })
-  }
+//   if (!session && isProtectedRouteWithProtectedMethod(req.nextUrl.pathname, req.method)) {
+//     return new NextResponse(JSON.stringify({ success: false, message: 'Unauthorized' }), {
+//       status: 401,
+//       headers: { 'content-type': 'application/json' }
+//     })
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: ['/api/users/:path*', '/api/rankings/:path*']
-}
+// export const config = {
+//   matcher: ['/api/users/:path*', '/api/rankings/:path*']
+// }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function middleware() {}
