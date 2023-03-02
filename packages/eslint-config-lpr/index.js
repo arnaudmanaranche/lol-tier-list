0272ed9e2aea560ddbf31988099acf8c45ac603f
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-03-02 12:59:58 +0100

    feat: add new eslint rules

--- packages/eslint-config-lpr/index.js
+++ packages/eslint-config-lpr/index.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
   plugins: ['simple-import-sort'],
   rules: {
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'simple-import-sort/exports': 2,
     'simple-import-sort/imports': [
       2,
       {
@@ -13,8 +17,6 @@ module.exports = {
           ['^.+\\.css$']
         ]
       }
-    ],
-    'simple-import-sort/exports': 2,
-    '@typescript-eslint/consistent-type-imports': 'error'
+    ]
   }
 }
