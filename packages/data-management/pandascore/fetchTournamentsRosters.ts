78f80ace69d1854d8acd75685c5e5bbc0d7b2561
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-05-29 19:48:41 +0200

    feat: optimize fetch tournaments

--- packages/data-management/pandascore/fetchTournamentsRosters.ts
+++ packages/data-management/pandascore/fetchTournamentsRosters.ts
@@ -1,6 +1,6 @@
 import { getPlaiceholder } from 'plaiceholder'
 
-import type { PLAYER, TEAM } from '@lpr/types'
+import type { TEAM } from '@lpr/types'
 
 import type { PandaScoreTournament } from './types'
 
@@ -14,41 +14,34 @@ export async function fetchPandascoreTournamentsRosters(
   const organizedTeams: TEAM[] = []
 
   const rosterPromises = tournamentIds.map(async (tournamentId) => {
-    const pandaScoreTournament: PandaScoreTournament = await fetch(
+    const response = await fetch(
       `https://api.pandascore.co/tournaments/${tournamentId}/rosters?token=${process.env.PANDASCORE_TOKEN}`
-    ).then((response) => response.json())
+    )
+    const { rosters } = (await response.json()) as PandaScoreTournament
 
-    for (const {
-      acronym,
-      id,
-      name: tournamentName,
-      players: unorganizedPlayers
-    } of pandaScoreTournament.rosters) {
-      const teamLogo = `https://${
-        process.env.NEXT_PUBLIC_SUPABASE_ID
-      }.supabase.co/storage/v1/object/public/${tournamentRegion.toLowerCase()}/${tournamentYear}/${
-        acronym?.toLowerCase() ?? ''
-      }.png`
-      const { base64 } = await getPlaiceholder(teamLogo)
+    await Promise.all(
+      rosters.map(async ({ acronym, id, name: tournamentName, players: unorganizedPlayers }) => {
+        const teamLogo = `https://${
+          process.env.NEXT_PUBLIC_SUPABASE_ID
+        }.supabase.co/storage/v1/object/public/${tournamentRegion.toLowerCase()}/${tournamentYear}/${
+          acronym?.toLowerCase() ?? ''
+        }.png`
+        const { base64 } = await getPlaiceholder(teamLogo)
 
-      const organizedPlayers: PLAYER[] = []
-      unorganizedPlayers.sort(
-        (a: PLAYER, b: PLAYER) => LINEUP_ORDER.indexOf(a.role) - LINEUP_ORDER.indexOf(b.role)
-      )
+        const organizedPlayers = unorganizedPlayers
+          .sort((a, b) => LINEUP_ORDER.indexOf(a.role) - LINEUP_ORDER.indexOf(b.role))
+          .map(({ id, name, role }) => ({ id, name, role }))
 
-      for (const { id, name, role } of unorganizedPlayers) {
-        organizedPlayers.push({ id, name, role })
-      }
-
-      organizedTeams.push({
-        id,
-        acronym,
-        name: tournamentName,
-        players: organizedPlayers,
-        logo: teamLogo,
-        logo_base64: base64
+        organizedTeams.push({
+          id,
+          acronym,
+          name: tournamentName,
+          players: organizedPlayers,
+          logo: teamLogo,
+          logo_base64: base64
+        })
       })
-    }
+    )
   })
 
   await Promise.all(rosterPromises)
